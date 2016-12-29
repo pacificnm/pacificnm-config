262e5a271e85aaffcd39adc066fa5efff1297f9b
Jaimie Garner <pacificnm@gmail.com>
2016-12-29 10:23:40 -0800

    updates readme and config

--- config/module.config.php
+++ config/module.config.php
@@ -10,7 +10,7 @@ return array(
     'module' => array(
         'Config' => array(
             'name' => 'Config',
-            'version' => '1.0.3',
+            'version' => '1.0.4',
             'install' => array(
                 'require' => array(),
                 'sql' => 'sql/config.sql'
